23a15d967e212ccab2fccbfbd2e76091b6c36bc8
Ben Pearson <79908678+MrBen89@users.noreply.github.com>
2022-03-28 18:52:42 +0900

    Add edit form to todo

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,6 @@
 import ListItem from "@mui/material/ListItem";
+import EditTodoForm from "./EditTodoForm";
+import useToggleState from "./hooks/useToggleState";
 import ListItemText from "@mui/material/ListItemText";
 import Checkbox from "@mui/material/Checkbox";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -6,19 +8,24 @@ import EditIcon from "@mui/icons-material/Edit";
 import IconButton from "@mui/material/IconButton";
 import ListItemSecondaryAction from "@mui/material/ListItemSecondaryAction";
 
-function Todo({ task, completed, id, removeTodo, toggleTodo }) {
+function Todo({ task, completed, id, removeTodo, toggleTodo, editTodo }) {
+    const [isEditing, toggle] = useToggleState();
     return(
         <ListItem>
+        {isEditing ? <EditTodoForm editTodo={editTodo} id={id} task={ task } toggleEditForm={ toggle }/>:
+            <>
             <Checkbox tabIndex={-1} checked={completed} onClick={() => toggleTodo(id)}/>
             <ListItemText sx={{textDecoration: completed ? "line-through" : "none"}} > {task} </ListItemText>
             <ListItemSecondaryAction >
-                <IconButton>
-                    <EditIcon aria-label="Edit" />
+                <IconButton aria-label="Edit" onClick={toggle}>
+                    <EditIcon />
                 </IconButton>
                 <IconButton aria-label="Delete" onClick={() => removeTodo(id)}>
                     <DeleteIcon />
                 </IconButton>
             </ListItemSecondaryAction>
+            </>
+            }
         </ListItem>
     )
 };
